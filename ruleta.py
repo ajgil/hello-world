ed2dc19f72db64eabc5d2887409f111b85bf8817
Toño <antoniojesusgil@gmail.com>
2018-11-20 17:22:03 +0100

    Update ruleta.py

--- ruleta.py
+++ ruleta.py
@@ -1,12 +1,8 @@
+# -*- coding: utf-8 -*-
 from math import sqrt
 from random import random
+import os
 import sys
-
-bankroll = 0
-apuesta = 0
-listaApuestas = []
-sec = 0
-tipoApuesta = False
 
 def apestasApuesta(bankroll,apuesta):
    if (apuesta >= bankroll):
@@ -14,77 +10,128 @@ def apestasApuesta(bankroll,apuesta):
        sys.exit(0)
    return
 
-def winBeneficio(win,beneficio):
-    if (win >= beneficio):
-        print("Has ganado mucho dineriiiiiitooo")
-        sys.exit(0)
+def menu():
+    '''
+    Limpiamos la pantalla
+    '''
+    os.system('cls') #Linux cls
+    print("Hola Roulettero, Bienvenido a apuesta o muere!")
+    print("Esta app indica la cantidad de iteraciones que necesitas para alcanzar tu objetivo, bribón!")   
+    #input("Presiona intro")
+   
+#bankroll, apuesta, sec = 0,0,0
+#listaApuestas = []
+#tipoApuesta = False
 
-print("Hola Roulettero, Bienvenido a apuesta o muere!")
-print("Esta app indica la cantidad de iteraciones que necesitas para alcanzar tu objetivo, bribón!")
-bankroll = int(input ("Indica tu presupuesto: "))
-apuesta = int(input("Indica cantidad a apostar: "))
-apestasApuesta(bankroll,apuesta)
-beneficio = int (input("¿Cuanto quieres ganar?: "))
-#input("Presiona intro")
-print("\n")
-print("Listado de secuencias de apuestas disponibles:\n----------------------------------")
-print("Fibonacci: Obtiene el siguiente número de la serie cuando pierde, resta 2 posiciones cuando gana")
-print("Martingala: Dobla la apuesta anterior siempre que hayas perdido, si gana no dobla")
-sec = int(input("Elige 1 para Fibonacci o 2 para Martingala: "))
-#input("Presiona intro")
-#print("\n")
-#print("Listado de apuestas:\n--------------------")
-#print("Apuesta a números pares escribiendo 'pares'")
-#print("Apuesta a números impares escribiendo 'impares'")
-#tipoApuesta = input("Escribe 'pares' o 'impares', ¿A qué apuestas?: ")
+def ganancias(bankroll,aux2,i):
+    if (bankroll >= aux2):
+        print("Has conseguido tu objetivo de ganar {} Euros".format(aux2))
+        print("Has ganado {}. Eso es mucho dineriiiiiitooo en {} apuestas".format(bankroll,i-1))
+        sys.exit(0)
 
 # Fibonnaci
 def F(n):
-   #fib = ((1+sqrt(5))**n-(1-sqrt(5))**n)/(2**n*sqrt(5))
-   #print("fibonacci apuesta: ", fib)
+   #fib = int(((1+sqrt(5))**n-(1-sqrt(5))**n)/(2**n*sqrt(5)))
    return ((1+sqrt(5))**n-(1-sqrt(5))**n)/(2**n*sqrt(5))
 
 # Martingala
 def M(n):
-   mart = n*2
-   print (mart)
-   return mart
+   return n*2
 
-# Apuestas rojo, negro, par, impar con el 0 -verde-
-def betstowin(bankroll, apuesta, beneficio):
-   i = 1
-   win = 0
-   print("Apuesta\tResultado Aleatorio\tGana/Pierde\t\tBankroll")
-      
-   while apuesta < bankroll:
-        winBeneficio(bankroll,beneficio)
-        aleatorio=random()
-        if aleatorio<0.45: # Rojo y par ganan
-            win=int(apuesta)*2
-            bankroll = bankroll + win
-            print("{}\t\t{}\t\tGANA\t{}\t\t{}".format(apuesta,str(aleatorio)[0:4],win,bankroll))
-        elif aleatorio<0.90: # Negro impar pierden
-            loose=int(apuesta)
-            bankroll = bankroll - loose 
-            print("{}\t\t{}\t\tPIERDE\t{}\t\t{}".format(apuesta,str(aleatorio)[0:4],loose,bankroll))
-            apuesta = int(F(apuesta))
-            print ("apuesta con fib", apuesta )
-        else: # Verde 0 pierde
-            loose=int(apuesta)
-            bankroll = bankroll - loose
-            print("{}\t\t{}\t\tCero - PIERDE\t{}\t\t{}".format(apuesta,str(aleatorio)[0:5],loose,bankroll))
-            apuesta = int(apuesta + F(apuesta))
-                        
-        i=i+1
-        #apuesta = int(apuesta + F(apuesta))
-        # print("ahora apuesas: ", apuesta)
+while True:
+    # Mostramos menu
+    #menu()
 
-        if (bankroll <= apuesta):
-            print("----- Has perdido todo tu dinero en {} tiradas -------".format(i))
-            return
+    # solicituamos una opción al usuario
+    bankroll = int(input ("Indica tu presupuesto: "))
+    apuesta = int(input("Indica cantidad a apostar: "))
+    apestasApuesta(bankroll,apuesta)
+    beneficio = int (input("¿Cuanto quieres ganar?: ")) # Cuanto quiero ganar
+    print("Listado de series / secuencias disponibles:\n----------------------------------")
+    print("Fibonacci: Obtiene el siguiente número de la serie cuando pierde, resta 2 posiciones cuando gana")
+    print("Martingala: Dobla la apuesta anterior siempre que hayas perdido, si gana vuelve a iniciar la secuencia")
+    sec = input("Elige 1 para Fibonacci o 2 para Martingala >> ")
+
+    if sec == "1":
+        print ("")
+        # implementamos fibonacci
+        def fib(bankroll, apuesta, beneficio):
+            i = 1
+            win = 0
+            aux = 1
+            aux2 = bankroll + beneficio
+            print("tirada\tApuesta\tResultado Aleatorio\tGana/Pierde\t\tBankroll\tFibonacci")
+            
+            while apuesta < bankroll:
+                ganancias(bankroll,aux2,i)
+                aleatorio=random()
+                if aleatorio<0.45: # Rojo y par ganan
+                    win=int(apuesta)
+                    bankroll = bankroll + win
+                    apuesta = int(F(aux - 2))
+                    if apuesta <= 0:
+                        apuesta = 1
+                    print("{}\t{}\t\t{}\t\tGANA\t\t{}\t{}\t\t{}".format(i,apuesta,str(aleatorio)[0:4],win,bankroll,aux))
+                elif aleatorio<0.90: # Negro impar pierden
+                    aux +=1
+                    loose=int(apuesta)
+                    bankroll = bankroll - loose 
+                    print("{}\t{}\t\t{}\t\tPIERDE\t\t{}\t{}\t\t{}".format(i,apuesta,str(aleatorio)[0:4],loose,bankroll,aux))
+                    apuesta = int(F(aux))
+                    #print("fibonacci {}".format(aux))
+                else: # Verde 0 pierde
+                    aux += 1
+                    loose=int(apuesta)
+                    bankroll = bankroll - loose
+                    print("{}\t{}\t\t{}\t\tCero - PIERDE\t{}\t{}\t\t{}".format(i,apuesta,str(aleatorio)[0:5],loose,bankroll,aux))
+                    apuesta = int(F(aux))                      
                 
-   print("-FIN DEL PROGRAMA, HAS NECESITADO {} TIRADAS -".format(i))
+                
+                i += 1
 
-#ruleta(10)
-#betstowin(bankroll)
-betstowin(bankroll,apuesta,beneficio)
+                if (bankroll <= apuesta):
+                    print("----- Has perdido todo tu dinero en {} tiradas -------".format(i-1))
+                    print("----- Te ha quedado un bankroll de {} aurelios -------".format(bankroll))
+                    return
+                        
+            print("-FIN DEL PROGRAMA, HAS NECESITADO {} TIRADAS -".format(i))
+        fib(bankroll, apuesta, beneficio)
+    elif sec == "2":
+        print("")
+        # implementamos martingala
+        def mart(bankroll, apuesta, beneficio):
+            i = 1
+            win = 0
+            aux = 1
+            aux2 = bankroll + beneficio
+            print("tirada\tApuesta\tResultado Aleatorio\tGana/Pierde\t\tBankroll\tMartingala")
+            while apuesta < bankroll:
+                ganancias(bankroll,aux2,i)
+                aleatorio=random()
+                if aleatorio<0.45: # Rojo y par ganan
+                    win=int(apuesta)
+                    bankroll = bankroll + win
+                    print("{}\t{}\t\t{}\t\tGANA\t\t{}\t{}\t\t{}".format(i,apuesta,str(aleatorio)[0:4],win,bankroll,aux))
+                    apuesta = 1
+                elif aleatorio<0.90: # Negro impar pierden
+                    loose=int(apuesta)
+                    bankroll = bankroll - loose 
+                    print("{}\t{}\t\t{}\t\tPIERDE\t\t{}\t{}\t\t{}".format(i,apuesta,str(aleatorio)[0:4],loose,bankroll,aux))
+                    apuesta *= 2
+                else: # Verde 0 pierde
+                    loose=int(apuesta)
+                    bankroll = bankroll - loose
+                    print("{}\t{}\t\t{}\t\tCero - PIERDE\t{}\t{}\t\t{}".format(i,apuesta,str(aleatorio)[0:5],loose,bankroll,aux))
+                    apuesta *= 2                    
+                
+                i += 1
+
+                if (bankroll <= apuesta):
+                    print("----- Has perdido todo tu dinero en {} tiradas -------".format(i-1))
+                    print("----- Te ha quedado un bankroll de {} aurelios -------".format(bankroll))
+                    return
+                        
+            print("-FIN DEL PROGRAMA, HAS NECESITADO {} TIRADAS -".format(i))
+        mart(bankroll, apuesta, beneficio)
+    else:
+        break
